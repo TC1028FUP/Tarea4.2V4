c0bde4eafcabc83e479ad0ea53da0d2187237ffd
fabyuribe <fabiola.uribe@tec.mx>
2021-09-07 00:32:42 +0000

    Probada ulam

--- assignments/26ConjeturaUlam/src/exercise.py
+++ assignments/26ConjeturaUlam/src/exercise.py
@@ -1,10 +1,8 @@
-# Escribe tu función aquí
-
-
 def main():
     num = int(input("Enter a number: "))
-    #escribe tu código abajo de esta línea
-    
+    # Comienzo el string resultado con el dato recibido
+    result = str(num)
+    # Escribe el código que falta para completar el string a desplegar    
 
 
 if __name__=='__main__':
